dfd1c0a10e7372933a2a1ddda1852f63a0844d6a
DinarSM <dinarsyibro1109@gmail.com>
2023-05-19 01:46:04 +0700

    perubahan title

--- app/Views/layouts/head.php
+++ app/Views/layouts/head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-         <title>Job board HTML-5 Template </title>
+         <title>Health Jobs </title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="manifest" href="site.webmanifest">
